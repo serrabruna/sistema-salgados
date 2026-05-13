a49487eb0137ae8dd838b72208b9a9f282dd64e1
serrabruna <brunaibserra@hotmail.com>
2026-05-13 15:39:38 -0300

    feat: criação de movimentacaoModel e movimentacaoRepository

---
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/model/Movimentacao.java
@@ -0,0 +1,22 @@
+package br.edu.ifsp.ads.sistema_salgados.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    private String sabor;
+    private Integer quantidade;
+    private Double valorTotal;
+    private LocalDateTime dataHora = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+}
---
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/repository/MovimentacaoRepository.java
@@ -0,0 +1,12 @@
+package br.edu.ifsp.ads.sistema_salgados.repository;
+
+import br.edu.ifsp.ads.sistema_salgados.model.Cliente;
+import br.edu.ifsp.ads.sistema_salgados.model.Movimentacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+    List<Movimentacao> findByCliente(Cliente cliente);
+    List<Movimentacao> findByClienteId(Long clienteId);
+}
