240aa337edac65133bb8a427532be01a08490e0e
serrabruna <brunaibserra@hotmail.com>
2026-05-13 15:16:43 -0300

    feat: inicialização do spring boot

---
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/SistemaSalgadosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ads.sistema_salgados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSalgadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSalgadosApplication.class, args);
+	}
+
+}
---
+++ sistema-salgados/src/test/java/br/edu/ifsp/ads/sistema_salgados/SistemaSalgadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ads.sistema_salgados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSalgadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
