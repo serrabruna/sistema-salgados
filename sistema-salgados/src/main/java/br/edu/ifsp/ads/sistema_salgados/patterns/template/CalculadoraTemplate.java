5f1300c23d84d77295788ab58d0618061ab99626
serrabruna <brunaibserra@hotmail.com>
2026-06-11 15:22:23 -0300

    desenvolvimento da classe abstrata do template method

---
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/patterns/template/CalculadoraTemplate.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.ads.sistema_salgados.patterns.template;
+
+public abstract class CalculadoraTemplate {
+    public final Double calcularPrecoFinal(Double precoBase, Integer quantidade) {
+        Double precoBruto = precoBase * quantidade;
+        Double desconto = 0.0;
+        
+        if (deveAplicarDesconto(quantidade)) {
+            desconto = aplicarDescontoEspecial(precoBruto);
+        }
+        
+        return precoBruto - desconto;
+    }
+
+    protected abstract boolean deveAplicarDesconto(Integer quantidade);
+    protected abstract Double aplicarDescontoEspecial(Double precoBruto);
+}
