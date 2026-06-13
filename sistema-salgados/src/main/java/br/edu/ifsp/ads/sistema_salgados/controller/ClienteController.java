11e007b90ba774bc312a1814eeb8535b68a719bd
serrabruna <brunaibserra@hotmail.com>
2026-06-13 15:19:56 -0300

    adicionando cors

--- sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/controller/ClienteController.java
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import br.edu.ifsp.ads.sistema_salgados.service.ClienteService;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Autowired
--- sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/controller/PedidoController.java
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/controller/PedidoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import br.edu.ifsp.ads.sistema_salgados.service.PedidoService;
 
 @RestController
 @RequestMapping("/api/pedidos")
+@CrossOrigin(origins = "*")
 public class PedidoController {
     @Autowired
     private MovimentacaoDao movimentacaoDao;
