db4b0d45ed7af673434175aa809e27244a727534
serrabruna <brunaibserra@hotmail.com>
2026-06-18 19:14:45 -0300

    correção em template

--- sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/patterns/template/CalculadoraPedidoProprio.java
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/patterns/template/CalculadoraPedidoProprio.java
@@ -2,7 +2,7 @@ package br.edu.ifsp.ads.sistema_salgados.patterns.template;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component //transforma a classe em um objeto que pode ser usado em qualquer lugar do sistema, sem precisar instanciar a classe
 public class CalculadoraPedidoProprio extends CalculadoraPrecoTemplate {
     @Override
     protected boolean deveAplicarDesconto(Integer quantidade) {
--- sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/patterns/template/CalculadoraPrecoTemplate.java
+++ sistema-salgados/src/main/java/br/edu/ifsp/ads/sistema_salgados/patterns/template/CalculadoraPrecoTemplate.java
@@ -1,8 +1,12 @@
 package br.edu.ifsp.ads.sistema_salgados.patterns.template;
 
+import br.edu.ifsp.ads.sistema_salgados.patterns.strategy.CalculoPrecoStrategy;
+import br.edu.ifsp.ads.sistema_salgados.patterns.strategy.PrecoVendaPadrao;
+
 public abstract class CalculadoraPrecoTemplate {
+    private CalculoPrecoStrategy estrategia = new PrecoVendaPadrao();
     public final Double calcularPrecoFinal(Double precoBase, Integer quantidade) {
-        Double precoBruto = precoBase * quantidade;
+        Double precoBruto = estrategia.calcular(precoBase, quantidade);
         Double desconto = 0.0;
         
         if (deveAplicarDesconto(quantidade)) {
